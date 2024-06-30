1b15df7dc3dbb3f4c4f8ab194ade25f5cf370135
xSamDx-web <sapozop@itsjapon.edu.ec>
2024-06-30 13:46:36 -0500

    Update Clase.py

--- Clase.py
+++ Clase.py
@@ -1,10 +1,7 @@
 class MiClase:
     def __init__(self, nombre):
         self.nombre = nombre
-
     def saludar(self):
         print(f"Hola, soy {self.nombre}!")
-
-# Ejemplo de uso
 objeto = MiClase("Sam")
 objeto.saludar()
