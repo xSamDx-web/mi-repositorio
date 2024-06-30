4cd43a8a1126058026e63fc4672ab1a19dd1d3fb
Your Name <you@example.com>
2024-06-30 14:47:51 -0500

    Cambio. Samuel Pozo

---
+++ Multiplicacion.py
@@ -0,0 +1,12 @@
+#Multiplicacion
+
+def multiplicar(a, b):
+ 
+    resultado = a * b
+    return resultado
+
+# Ejemplo de uso de la función
+x = 5
+y = 3
+resultado_multiplicacion = multiplicar(x, y)
+print(f"El resultado de {x} * {y} es: {resultado_multiplicacion}")
